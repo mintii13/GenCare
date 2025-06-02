a5ea47b4b66e23d5aea58a9f3d3198b502268c3b
Khuu Trong Quan <khuutrongquan220405@gmail.com>
2025-06-02 09:39:33 +0700

    Change folder code register and googleSignin

--- backend/src/repositories/userRepository.ts
+++ backend/src/repositories/userRepository.ts
@@ -22,39 +22,14 @@ export class UserRepository {
         }
     }
 
-    public static async insertGoogle(profile: any): Promise<Express.User>{
-        const email = profile.emails[0]?.value || null;
-        const full_name = profile.name.givenName  + " " + profile.name.familyName;
-        const registration_date = new Date();
-        const updated_date = new Date();
-        const status = true;
-        const email_verified = true;
-        const role = 'customer';
-        const googleId = profile.id;
-
-        let user = await User.findOne({googleId});
-        if (!user){
-            user = await User.create({email, full_name, registration_date, updated_date, status, email_verified, role, googleId})
-        }
-        return user;
+    public static async insertUser(user: Partial<IUser>)
+    {
+        try {
+            return await User.create(user);
+        } catch (error) {
+            console.error('Error insert user:', error);
+            throw error;
+        }  
     }
-
-    public static async insertMyApp(user: {
-        email: string;
-        password?: string;
-        full_name: string;
-        phone?: string;
-        date_of_birth?: Date;
-        gender?: string;
-        registration_date: Date;
-        updated_date: Date;
-        last_login?: Date;
-        status: boolean;
-        email_verified: boolean;
-        role: 'customer' | 'consultant' | 'staff' | 'admin';
-        googleId?: string;
-    }) {
-        const newUser = new User(user);
-        return await newUser.save();
-    }
+    
 }
