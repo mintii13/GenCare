20b79ecc2c4c3bb582dd2bafb61a3f412f636757
Khuu Trong Quan <khuutrongquan220405@gmail.com>
2025-06-16 09:08:37 +0700

    Sti-mainflow

--- backend/src/models/StiAuditLog.ts
+++ backend/src/models/StiAuditLog.ts
@@ -1,24 +1,28 @@
 // models/AuditLog.ts
 import mongoose, { Schema, Document } from 'mongoose';
 
+export type TargetType = 'StiOrder' | 'StiPackage' | 'StiTest';
+
 export interface IStiAuditLog extends Document {
-  user_id: mongoose.Types.ObjectId;
-  action: string;
-  order_id: mongoose.Types.ObjectId;
-  before?: any;
-  after?: any;
-  timestamp: Date;
-  notes?: string;
+    target_type: TargetType;                            
+    target_id: mongoose.Types.ObjectId;
+    user_id: mongoose.Types.ObjectId;
+    action: string;
+    before?: any;
+    after?: any;
+    timestamp: Date;
+    notes?: string;
 }
 
 const stiAuditLogSchema = new Schema<IStiAuditLog>({
-  user_id: { type: Schema.Types.ObjectId, required: true},
-  action: { type: String, required: true },
-  order_id: { type: Schema.Types.ObjectId, ref: 'StiOrder', required: true },
-  before: { type: Schema.Types.Mixed },
-  after: { type: Schema.Types.Mixed },
-  timestamp: { type: Date, default: Date.now },
-  notes: { type: String },
+    target_type: {type: String, enum: ['StiOrder', 'StiPackage', 'StiTest'], required: true},
+    target_id: {type: Schema.Types.ObjectId, required: true},
+    user_id: { type: Schema.Types.ObjectId, required: true},
+    action: { type: String, required: true },
+    before: { type: Schema.Types.Mixed },
+    after: { type: Schema.Types.Mixed },
+    timestamp: { type: Date, default: Date.now },
+    notes: { type: String },
 });
 
 export const StiAuditLog = mongoose.model<IStiAuditLog>('AuditLog', stiAuditLogSchema);
